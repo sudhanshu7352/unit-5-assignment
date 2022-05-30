d19d7a46982d7ee7757eb45f1d58b7b086230e3b
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-05-30 21:11:31 +0530

    bill divsion

--- dsa ques/30 may.js
+++ dsa ques/30 may.js
@@ -27,3 +27,40 @@ function dayOfProgrammer(year) {
        return "26.09.1918"
    }
 }
+/////////bill division 
+// Complete the bonAppetit function in the editor below. It should print Bon Appetit if the bill is fairly split. Otherwise, it should print the integer amount of money that Brian owes Anna.
+
+// bonAppetit has the following parameter(s):
+
+// bill: an array of integers representing the cost of each item ordered
+// k: an integer representing the zero-based index of the item Anna doesn't eat
+// b: the amount of money that Anna contributed to the bill
+
+// Sample Input 0
+
+// 4 1
+// 3 10 2 9
+// 12
+// Sample Output 0
+
+// 5
+function bonAppetit(bill, k, b) {
+    // Write your code here
+  let sum =0
+  
+  for(let i=0;i<bill.length;i++){
+      if(i==k){
+          continue
+      }else{
+          sum +=bill[i]
+      }
+  }
+  let ans 
+  if(sum/2 ==b){
+      ans ="Bon Appetit"
+  }else{
+     ans =b- sum/2
+  }
+ console.log(ans)
+  // return ans
+}
