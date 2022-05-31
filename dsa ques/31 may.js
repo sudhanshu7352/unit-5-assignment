7ef7b491c88330310867cb24a923ae88b476029f
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-05-31 20:22:36 +0530

    page counts

--- dsa ques/31 may.js
+++ dsa ques/31 may.js
@@ -44,3 +44,41 @@ function sockMerchant(n, ar) {
   }
   return count
 }
+//A teacher asks the class to open their books to a page number. A student can either start turning pages from the front of the book or from the back of the book. They always turn pages one at a time. When they open the book, page   1 is always on the right side:
+
+// image
+
+// When they flip page 1, they see pages  and . Each page except the last page will always be printed on both sides. The last page may only be printed on the front, given the length of the book. If the book is  pages long, and a student wants to turn to page , what is the minimum number of pages to turn? They can start at the beginning or the end of the book.
+
+// Given n and p, find and print the minimum number of pages that must be turned in order to arrive at page .
+
+function pageCount(n, p) {
+    // Write your code here
+     let s =0
+     let e=0
+     let page =1
+     
+     while(page<p){
+         s++
+         page+=2
+     }
+     if(n%2 !==0){
+         page =n-1
+     }else{
+         page=n
+     }
+     while(page>p){
+         e++
+         page-=2
+     }
+     if(s>e){
+        // console.log(e)
+         return e
+     }else{
+        // console.log(s)
+         return s
+     }
+    //  while(temp<n){
+         
+    //  }
+}
