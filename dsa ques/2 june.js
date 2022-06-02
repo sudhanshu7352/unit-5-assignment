f0115b548452d97602d38b16f0207808f3c8c67b
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-06-02 20:55:26 +0530

    picking numbers

--- dsa ques/2 june.js
+++ dsa ques/2 june.js
@@ -54,3 +54,21 @@ function formingMagicSquare(s) {
   }
   return cost;
 }
+//picking numbers
+//Given an array of integers, find the longest subarray where the absolute difference between any two elements is less than or equal to .
+
+function pickingNumbers(a) {
+    // Write your code here
+ let sorted_arr= a.sort((a,b)=>a-b)
+ let i=0
+ let j=1
+  while(i<sorted_arr.length && j<sorted_arr.length){
+      if((Math.abs(sorted_arr[i]-sorted_arr[j])) >1 ){
+          i++
+          j++
+      }else{
+          j++
+      }
+  }
+  return Math.abs(i-j)
+}
