7481a5c97f5ddba9c760b10514710a09cabf8e67
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-06-04 21:30:11 +0530

    count in array of objects

--- dsa ques/4 june.js
+++ dsa ques/4 june.js
@@ -102,3 +102,13 @@ function getSecondLargest(nums) {
         }
     }
 }
+function getCount(objects) {
+    // console.log(objects[0])
+     let count=0
+     for(let i=0;i<objects.length;i++){
+         if(objects[i].x ==objects[i].y){
+             count++
+         }
+     }
+     return count
+ }
