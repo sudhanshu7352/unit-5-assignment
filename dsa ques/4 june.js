736504903b308bd69b6d34d27445da1082d0e0e8
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-06-04 20:38:08 +0530

    vowels and consonenets

--- dsa ques/4 june.js
+++ dsa ques/4 june.js
@@ -70,9 +70,22 @@ function beautifulDays(i, j, k) {
   }
   return count
 }
-function factorial(n){
-    if(n==0){
-        return 1
+// function factorial(n){
+//     if(n==0){
+//         return 1
+//     }
+//     return n*factorial(n-1)
+// }
+function vowelsAndConsonants(s) {
+    
+    for(let i=0;i<s.length;i++){
+        if(s[i] =='a'|| s[i] =='e'||s[i] =='i'||s[i] =='o'||s[i] =='u'){
+            console.log(s[i])
+        }
     }
-    return n*factorial(n-1)
+    for(let i=0;i<s.length;i++){
+        if(s[i] !=='a' && s[i] !=='e' && s[i] !=='i' && s[i] !=='o' && s[i] !=='u'){
+            console.log(s[i])
+        }
+    }
 }
