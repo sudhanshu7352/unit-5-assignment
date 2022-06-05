4ce17fececcb4ba7c3e455af704a5939029bd3f0
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-06-05 18:23:29 +0530

    class inheritance

--- dsa ques/5 june.js
+++ dsa ques/5 june.js
@@ -8,7 +8,7 @@ class Polygon{
     constructor(side){
         this.side =side
     }
-    perimeter(side){
+    perimeter(){
         let sum=0
         for(let i=0;i<this.side.length;i++){
             sum +=this.side[i]
@@ -16,3 +16,25 @@ class Polygon{
         return sum
     }
 }
+//class inheritance
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+}
+
+/*
+ *  Write code that adds an 'area' method to the Rectangle class' prototype
+ */
+ Rectangle.prototype.area=function(){
+     return this.w*this.h
+ }
+/*
+ * Create a Square class that inherits from Rectangle and implement its class constructor
+ */
+  class Square extends Rectangle{
+      constructor(a){
+          super(a,a)
+      }
+  }
