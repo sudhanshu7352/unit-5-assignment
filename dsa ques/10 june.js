f63902017ce34468f9ac1e99e33815b75964e59e
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-06-10 19:19:26 +0530

    string

--- dsa ques/10 june.js
+++ dsa ques/10 june.js
@@ -66,3 +66,40 @@ function mergeSort(arr,l, r){
     mergeSort(arr,m+1,r);
     merge(arr,l,m,r);
 }
+
+//longest substring
+var longestPalindrome = function(string) {
+
+    var length = string.length;
+    var result = "";
+  
+    var centeredPalindrome = function(left, right) {
+      while (left >= 0 && right < length && string[left] === string[right]) {
+        //expand in each direction.
+        left--;
+        right++;
+      }
+  
+      return string.slice(left + 1, right);
+    };
+  
+    for (var i = 0; i < length - 1; i++) {
+      var oddPal = centeredPalindrome(i, i + 1);
+  
+      var evenPal = centeredPalindrome(i, i);
+  
+      if (oddPal.length > 1)
+        console.log("oddPal: " + oddPal);
+      if (evenPal.length > 1)
+        console.log("evenPal: " + evenPal);
+  
+      if (oddPal.length > result.length)
+        result = oddPal;
+      if (evenPal.length > result.length)
+        result = evenPal;
+    }
+    return "the palindrome is: " + result + " and its length is: " + result.length;
+  };
+  
+  console.log(longestPalindrome("n"));
+  
